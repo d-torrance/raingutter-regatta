0a685a8e1842862f00498bf5fae91b0b072ecf32
Doug Torrance <dtorrance@piedmont.edu>
2022-04-22 13:02:57 -0400

    Initial commit

---
+++ raingutter-regatta.py
@@ -0,0 +1,43 @@
+import double_elimination
+import sys
+
+class Regatta:
+    def __init__(self, participant_file):
+        with open(participant_file) as f:
+            participants = [line.strip() for line in f]
+        self.tournament = double_elimination.Tournament(participants)
+
+    def run_match(self, match):
+        (left, right) = tuple(match.get_participants())
+        participants = {
+            "a" : left.get_competitor(),
+            "b" : right.get_competitor()}
+        print(f"(a) {participants['a']}")
+        print(f"(b) {participants['b']}\n")
+        sure = "n"
+        while sure != "y":
+            winner = ""
+            while not(winner == 'a' or winner == 'b'):
+                winner = input("Winner? (a/b): ")
+            print(f"The winner is {participants[winner]}.")
+            sure = input("Is this correct? (y/n) ")
+        match.set_winner(participants[winner])
+
+    def run_tournament(self):
+        print("Beginning the Raingutter Regatta!")
+        print("=================================\n")
+        round = 1
+        while len(self.tournament.get_active_matches()) > 0:
+            print(f"Round {round}")
+            print((len(str(round)) + 6) * '-')
+            current_match = self.tournament.get_active_matches()[0]
+            self.run_match(current_match)
+            round += 1
+            print()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("expected a single filename")
+    regatta = Regatta(sys.argv[1])
+    regatta.run_tournament()
